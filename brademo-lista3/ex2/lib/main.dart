18f0ce0b351953b26b743b9bd12c35abb28f947a
guedespedro <pedroguazevedo123@gmail.com>
2026-03-30 20:24:23 -0300

    correção dos diretórios e adição do exercício 2

--- brademo-lista3/ex2/lib/main.dart
+++ brademo-lista3/ex2/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +10,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const LoginScreen(),
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFE0E0E0),
+      ),
+      home: Scaffold(
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Center(
+              child: SizedBox(
+                width: 250,
+                height: 200,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlue,
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Theme(
+                data: ThemeData(
+                  scaffoldBackgroundColor: Colors.grey[400],
+                ),
+                child: Builder(
+                  builder: (context) {
+                    return Container(
+                      width: 400,
+                      height: 350,
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      child: Center(
+                        child: Theme(
+                          data: Theme.of(context).copyWith(
+                            primaryColor: Colors.yellow,
+                          ),
+                          child: Builder(
+                            builder: (context) {
+                              return Container(
+                                width: 250,
+                                height: 200,
+                                color: Theme.of(context).primaryColor,
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
